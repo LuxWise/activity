a013db8723a81b2ad2a0f230f1bb74d8ae979c97
LuxWise <jcsanchez.martinez,2020@gmail.com>
2024-08-06 14:48:11 -0500

    list

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,77 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ListView
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), View.OnClickListener {
+    lateinit var etCiudad:EditText
+    lateinit var btAdicionar:Button
+    lateinit var btEliminar:Button
+    lateinit var lvCiudad:ListView
+
+    lateinit var adapterCiudades: ArrayAdapter<String>
+    lateinit var listCiudad:MutableList<String>
+    lateinit var sCiudad:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+
+        btAdicionar= findViewById(R.id.butnAdicionar)
+        etCiudad= findViewById(R.id.editTextCiudad)
+        lvCiudad= findViewById(R.id.LvCiudades)
+        btAdicionar.setOnClickListener(this)
+        btEliminar= findViewById(R.id.delete)
+
+        listCiudad= arrayListOf(
+            "Bogota",
+            "Cali",
+            "Medellin",
+        )
+
+        adapterCiudades= ArrayAdapter(this, android.R.layout.simple_list_item_1, listCiudad)
+        adapterCiudades.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        lvCiudad.adapter=adapterCiudades
+
+        var selectedView: View? = null
+
+        lvCiudad.setOnItemClickListener { parent, view, position, id ->
+            sCiudad = listCiudad[position]
+
+            selectedView?.setBackgroundColor(Color.TRANSPARENT)
+            view.setBackgroundColor(Color.parseColor("#FFC107"))
+            selectedView = view
+        }
+
+        btEliminar.setOnClickListener {
+            listCiudad.remove(sCiudad)
+            adapterCiudades.notifyDataSetChanged()
+        }
+
+        btAdicionar.setOnClickListener {
+            sCiudad= etCiudad.text.toString()
+
+            if (sCiudad.isEmpty()){
+                etCiudad.error="Debe ingresar una ciudad"
+                return@setOnClickListener
+            } else if (listCiudad.contains(sCiudad)){
+                etCiudad.error="La ciudad ya existe"
+                return@setOnClickListener
+            }
+
+            listCiudad.add(sCiudad)
+            adapterCiudades.notifyDataSetChanged()
+        }
+
+
 
 //        setContentView(R.layout.activity_main)
 //        val name: TextView = findViewById(R.id.name)
@@ -35,5 +97,9 @@ class MainActivity : ComponentActivity() {
 //            text.text = "${name.text} ${lastname.text}"
 //        }
 
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
     }
 }
